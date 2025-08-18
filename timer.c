feacdba0fb7f3802461ecf813529adccdb6a199d
lucasplaygaemes <lucasmoura27271@gmail.com>
2025-08-18 14:02:06 -0300

    Timer total added, and a simple error  also corrected

--- timer.c
+++ timer.c
@@ -55,7 +55,7 @@ void display_work_summary() {
         getchar();
         return;
     }
-
+    long timer_total = 0;
     long today_total = 0;
     long week_total = 0;
     long month_total = 0;
@@ -69,11 +69,10 @@ void display_work_summary() {
     while (fgets(line, sizeof(line), file)) {
         char* date_part = strtok(line, ",");
         char* seconds_part = strtok(NULL, ",");
-
         if (date_part && seconds_part) {
             long seconds = atol(seconds_part);
             struct tm log_time = {0};
-
+            timer_total += seconds;
             // Parse the YYYY-MM-DD format
             sscanf(date_part, "%d-%d-%d", &log_time.tm_year, &log_time.tm_mon, &log_time.tm_mday);
 
@@ -108,33 +107,31 @@ void display_work_summary() {
         }
     }
     fclose(file);
-
     char date_str[100];
     // Note: The output language of strftime depends on the system's locale.
     strftime(date_str, sizeof(date_str), "%A, %B %d, %Y", current_time);
 
-    char today_buf[50], week_buf[50], month_buf[50], sem_buf[50], year_buf[50];
+    char today_buf[50], total_buf[50], week_buf[50], month_buf[50], sem_buf[50], year_buf[50];
     format_duration(today_total, today_buf, sizeof(today_buf));
     format_duration(week_total, week_buf, sizeof(week_buf));
     format_duration(month_total, month_buf, sizeof(month_buf));
     format_duration(semester_total, sem_buf, sizeof(sem_buf));
     format_duration(year_total, year_buf, sizeof(year_buf));
-
+    format_duration(timer_total, total_buf, sizeof(total_buf));
+    
     system("clear");
     printf("--- Work Time Report ---\n");
     printf("Date: %s\n\n", date_str);
-    printf("     Today: %s\n", today_buf);
-    printf("      Week: %s\n", week_buf);
-    printf("     Month: %s\n", month_buf);
-    printf("  Semester: %s\n", sem_buf);
-    printf("      Year: %s\n", year_buf);
+    printf("      Today: %s\n", today_buf);
+    printf("       Week: %s\n", week_buf);
+    printf("      Month: %s\n", month_buf);
+    printf("   Semester: %s\n", sem_buf);
+    printf("       Year: %s\n", year_buf);
+    printf("Total Timer: %s\n", total_buf);
     printf("\n--------------------------\n");
     printf("\nPress Enter to return to the editor...");
 
     // Clear input buffer and wait for Enter
     int c;
     while ((c = getchar()) != '\n' && c != EOF) { }
-    if (c != EOF) {
-        getchar();
-    }
 }
